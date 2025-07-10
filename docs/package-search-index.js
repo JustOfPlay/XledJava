bfd9724e290f76098e3b3c0bbfbf5023aa8d389e
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2025-07-10 22:21:54 +0200

    Fixes docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"},{"l":"de.justofplay.xled"},{"l":"de.justofplay.xled.utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.justofplay.xled"},{"l":"de.justofplay.xled.utils"}];updateSearchResults();
